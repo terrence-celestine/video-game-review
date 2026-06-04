caf55b81f006a67bff1219f732634cd49b00c0b7
Terrence <terrence.butler89@gmail.com>
2026-06-03 19:02:31 -0700

    tmp save

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,9 +5,11 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
